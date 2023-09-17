ac8a5ac282a2f069824225137cf35b30860c06f0
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-09-17 18:08:32 -0400

    added code for indexDB, updated our indexDb

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = () => {
         inject: true,
         name: 'Just Another Text Editor',
         short_name: 'J.A.T.E',
-        description: 'Takes notes with JavaScript syntax highlighting!',
+        description: 'A simple text editor that saves your notes to your browser.',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
